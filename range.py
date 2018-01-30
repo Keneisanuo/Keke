661727a43c1e24a7ce79e8cad3280ed9b50e47bf
kenei <avibuno27@gmail.com>
2018-01-30 10:43:48 +0530

    "range continue and break"

---
+++ range.py
@@ -0,0 +1,11 @@
+# break statement
+for n in range(10):
+    if n==6:
+        break
+    print(n)
+
+ # continue statement
+for n in range(1,10,1):
+    if(n%2==0):
+        print(n,"is even number")
+        continue
